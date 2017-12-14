ea2c8cca38d536ee8b82cdb546748a9bd56f10ca
ostrokach <ostrokach@gmail.com>
2017-12-14 11:59:14 -0500

    Don't pass include= to find_packages

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     author="Alexey Strokach",
     author_email='alex.strokach@utoronto.ca',
     url='https://gitlab.com/kimlab/pagnn',
-    packages=find_packages(include=['pagnn']),
+    packages=find_packages(),
     entry_points={'console_scripts': ['pagnn=pagnn.cli:main']},
     include_package_data=True,
     install_requires=requirements,
