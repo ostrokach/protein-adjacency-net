4d88ba0bfa79512c6496f5176adb574f921328f9
ostrokach <ostrokach@gmail.com>
2018-02-10 18:24:28 -0500

    Remove GPUtil dependency

--- pagnn/gpu.py
+++ pagnn/gpu.py
@@ -1,9 +1,13 @@
+import io
 import logging
 import os
+import shlex
+import subprocess
+from typing import List
 
-import GPUtil
 import numba
 import numba.cuda
+import pandas as pd
 
 from . import settings
 
@@ -21,9 +25,48 @@ def init_gpu(gpu_idx: int = None) -> None:
     """Specify which GPU should be used (or select the least active one)."""
     assert settings.CUDA
     if gpu_idx is None:
-        device_ids = GPUtil.getAvailable(order='first', limit=1, maxLoad=0.5, maxMemory=0.5)
+        device_ids = get_available_gpus(max_load=0.5, max_memory=0.5)
         device_id = ','.join(str(i) for i in device_ids)
     else:
         device_id = str(gpu_idx)
     os.environ['CUDA_VISIBLE_DEVICES'] = device_id
     logger.info("Running on GPU number %s.", os.environ['CUDA_VISIBLE_DEVICES'])
+
+
+def get_available_gpus(max_load: float = 0.5, max_memory: float = 0.5) -> List[int]:
+    """Get a list of GPUs with load under the given load requirements.
+
+    Args:
+        max_load: Max fraction of GPU cycles used.
+        max_memory: Max fractio of GPU memory used.
+
+    Returns:
+        A list of GPU ids for GPUs which meet the load requirements.
+    """
+    assert 0 <= max_load <= 1
+    assert 0 <= max_memory <= 1
+
+    columns = [
+        'index', 'utilization.gpu', 'memory.total', 'memory.used', 'memory.free', 'driver_version',
+        'name', 'gpu_serial', 'display_active', 'display_mode'
+    ]
+
+    system_command = f"nvidia-smi --query-gpu={','.join(columns)} --format=csv,noheader,nounits"
+
+    proc = subprocess.run(
+        shlex.split(system_command),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True)
+
+    buf = io.StringIO()
+    buf.write(proc.stdout)
+    buf.seek(0)
+
+    df = pd.read_csv(buf, names=columns)
+    df['memory_utilization'] = df['memory.used'] / df['memory.total']
+    df = df \
+        .sort_values(['memory.free'], ascending=False) \
+        .sort_values(['utilization.gpu'], ascending=True)
+    df = df[((df['utilization.gpu'] / 100) <= max_load) & (df['memory_utilization'] <= max_memory)]
+    return df['index'].tolist()
