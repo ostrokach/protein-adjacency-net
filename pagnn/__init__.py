6297dfb88917aaa80ad3fdae32542cbd3d25c561
ostrokach <ostrokach@gmail.com>
2018-02-27 16:05:58 -0500

    Bump version: 0.1.6.dev → 0.1.6

--- pagnn/__init__.py
+++ pagnn/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Alexey Strokach"""
 __email__ = 'alex.strokach@utoronto.ca'
-__version__ = '0.1.6.dev'
+__version__ = '0.1.6'
 __all__ = [
     'settings', 'types', 'exc', 'models', 'scripts',
 ]
