81041d9ed32569e820f1c085cf0285ffc17bd0e9
ostrokach <ostrokach@gmail.com>
2018-04-19 20:15:04 -0400

    Make it possibe to use AE Apply Extra as a GAN

--- pagnn/models/ae_seqadj_applyextra.py
+++ pagnn/models/ae_seqadj_applyextra.py
@@ -1,6 +1,7 @@
+import enum
 import logging
+from typing import Optional, Union
 
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,39 +16,65 @@ from .ae_sequential import SequentialMod
 logger = logging.getLogger(__name__)
 
 
+class NetworkMode(enum.Enum):
+    AE = enum.auto()
+    DISCRIMINATOR = enum.auto()
+    GENERATOR = enum.auto()
+
+
 class AESeqAdjApplyExtra(nn.Module):
 
     def __init__(self,
-                 n_layers,
-                 bottleneck_size,
-                 input_size=20,
-                 hidden_size=64,
-                 kernel_size=3,
-                 stride=2,
-                 padding=1,
-                 bias=False):
+                 mode: Union[NetworkMode, str],
+                 n_layers: int = 4,
+                 n_convs: int = 3,
+                 input_size: int = 20,
+                 hidden_size: int = 64,
+                 bottleneck_size: int = 16,
+                 kernel_size: int = 3,
+                 stride: int = 2,
+                 padding: int = 1,
+                 bias: bool = False,
+                 encoder_network: Optional['AESeqAdjApplyExtra'] = None) -> None:
         super().__init__()
 
+        self.mode = self._to_network_mode(mode)
         self.n_layers = n_layers
-        self.n_convs = 3
+        self.n_convs = n_convs
+        self.input_size = input_size
+        self.hidden_size = hidden_size
         self.bottleneck_size = bottleneck_size
-
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
+        self.bias = bias
 
-        print('kernel size 3 norm')
+        self.bottleneck_features = self.hidden_size * 2**(self.n_layers - 1)
 
-        # === Encoder ===
+        if self.mode in [NetworkMode.AE, NetworkMode.DISCRIMINATOR]:
+            self._configure_encoder()
+        if self.mode in [NetworkMode.AE, NetworkMode.GENERATOR]:
+            self._configure_decoder(encoder_network)
+
+    def _to_network_mode(self, mode):
+        if isinstance(mode, str):
+            for nm in NetworkMode:
+                if mode.upper() == nm.name:
+                    mode = nm
+                    break
+        assert isinstance(mode, NetworkMode)
+        return mode
+
+    def _configure_encoder(self):
         conv_kwargs = dict(
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            bias=self.bias,
         )
-        input_channels = input_size
-        for i in range(0, n_layers):
-            output_channels = int(input_channels * 2) if i > 0 else hidden_size
+        input_channels = self.input_size
+        for i in range(0, self.n_layers):
+            output_channels = int(input_channels * 2) if i > 0 else self.hidden_size
             negative_slope = 0.2 if i == 0 else 0.01
             # Input
             if i == 0:
@@ -75,7 +102,7 @@ class AESeqAdjApplyExtra(nn.Module):
             setattr(self, f'encoder_1_{i}',
                     SequentialMod(
                         SequenceConv(output_channels // 2, output_channels, **conv_kwargs),))
-            if i < (n_layers - 1):
+            if i < (self.n_layers - 1):
                 setattr(self, f'encoder_post_{i}',
                         nn.Sequential(
                             nn.LeakyReLU(negative_slope, inplace=True),
@@ -85,28 +112,36 @@ class AESeqAdjApplyExtra(nn.Module):
                 setattr(self, f'encoder_post_{i}', nn.Sequential())
             input_channels = output_channels
 
-        # === Linear ===
         if self.bottleneck_size > 0:
             self.linear_in = nn.Linear(2048, self.bottleneck_size, bias=True)
-            self.linear_out = nn.Linear(self.bottleneck_size, 2048, bias=True)
             self.conv_in = nn.Conv1d(
                 512, self.bottleneck_size, kernel_size=4, stride=4, padding=0, bias=True)
+
+        return input_channels
+
+    def _configure_decoder(self, encoder_net=None):
+        if encoder_net is None:
+            encoder_net = self
+
+        input_channels = self.bottleneck_features
+
+        if self.bottleneck_size > 0:
+            self.linear_out = nn.Linear(self.bottleneck_size, 2048, bias=True)
             self.conv_out = nn.Conv1d(
                 self.bottleneck_size, 512 * 4, kernel_size=1, stride=1, padding=0, bias=True)
 
-        # === Decoder ===
         convt_kwargs = dict(
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding - 1,
-            bias=bias,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding - 1,
+            bias=self.bias,
         )
-        for i in range(n_layers - 1, -1, -1):
-            output_channels = input_channels // 2 if i > 0 else input_size
-            if i < (n_layers - 1):
+        for i in range(self.n_layers - 1, -1, -1):
+            output_channels = input_channels // 2 if i > 0 else self.input_size
+            if i < (self.n_layers - 1):
                 setattr(self, f'decoder_pre_{i}',
                         nn.Sequential(
-                            nn.LeakyReLU(negative_slope, inplace=True),
+                            nn.ReLU(inplace=True),
                             nn.BatchNorm1d(input_channels),
                         ))
             else:
@@ -120,9 +155,10 @@ class AESeqAdjApplyExtra(nn.Module):
             if i < self.n_convs:
                 setattr(self, f'decoder_1_{i}',
                         SequentialMod(
-                            nn.LeakyReLU(negative_slope, inplace=True),
+                            nn.ReLU(inplace=True),
                             nn.BatchNorm1d(input_channels // 4),
-                            AdjacencyConvTranspose(getattr(self, f'encoder_0_{i}')[0].spatial_conv),
+                            AdjacencyConvTranspose(
+                                getattr(encoder_net, f'encoder_0_{i}')[0].spatial_conv),
                         ))
             else:
                 setattr(self, f'decoder_1_{i}', SequentialMod())
@@ -142,8 +178,20 @@ class AESeqAdjApplyExtra(nn.Module):
 
     def forward(self, seq, adjs):
         x = seq
+        if self.mode in [NetworkMode.AE, NetworkMode.DISCRIMINATOR]:
+            x = self._forward_encoder(x, adjs)
+        if self.mode in [NetworkMode.AE, NetworkMode.GENERATOR]:
+            num_aa = sum(adj[0].shape[1] for adj in adjs)
+            shape_in_range = ((x.shape[1] *
+                               (x.shape[2] - 1) * 0.9) <= (num_aa * self.bottleneck_size / 64) <=
+                              (x.shape[1] * x.shape[2] * 1.1))
+            assert shape_in_range, (x.shape, num_aa / 64 * self.bottleneck_size)
+            x = self._forward_decoder(x, adjs)
+        return x
 
-        # Encode
+    def _forward_encoder(self, seq, adjs):
+        x = seq
+
         for i in range(self.n_layers):
             x = getattr(self, f'encoder_pre_{i}')(x)
             x_adj = x[:, x.shape[1] // 2:, :]
@@ -153,31 +201,27 @@ class AESeqAdjApplyExtra(nn.Module):
             x = getattr(self, f'encoder_post_{i}')(x)
             logger.debug(f'{i}, {x.shape}')
 
-        # === Linear ===
         if self.bottleneck_size > 0:
-
             pad_amount = padding_amount(x, 2048)  # 4 x 512
             if pad_amount:
                 x = F.pad(x, (0, pad_amount))
-
-            n_features = x.shape[1]
             x = reshape_internal_dim(x, 1, 512)
-
             x = self.conv_in(x)
             # x = self.linear_in(x.transpose(1, 2).contiguous())
 
-            assert 0.9 < (np.prod(x.shape) / (seq.shape[2] / 64 * self.bottleneck_size)) <= 1.1, \
-                (x.shape[1:], seq.shape[2] / 64 * self.bottleneck_size)
+        return x
 
+    def _forward_decoder(self, seq, adjs):
+        x = seq
+
+        if self.bottleneck_size > 0:
             # x = self.linear_out(x).transpose(2, 1).contiguous()
             x = self.conv_out(x)
+            x = reshape_internal_dim(x, 1, self.bottleneck_features)
+            expected_length = sum(adj[self.n_layers].shape[1] for adj in adjs)
+            if x.shape[2] > expected_length:
+                x = x[:, :, :expected_length]
 
-            x = reshape_internal_dim(x, 1, n_features)
-
-            if pad_amount:
-                x = x[:, :, :-pad_amount]
-
-        # Decode
         for i in range(self.n_layers - 1, -1, -1):
             x = getattr(self, f'decoder_pre_{i}')(x)
             x = getattr(self, f'decoder_0_{i}')(x, i, adjs)
