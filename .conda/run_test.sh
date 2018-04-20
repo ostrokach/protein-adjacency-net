1a3dcfcc73f01d227206d37f1fc605574460ebc0
ostrokach <ostrokach@gmail.com>
2018-04-20 17:59:07 -0400

    Report coverage

--- .conda/run_test.sh
+++ .conda/run_test.sh
@@ -2,10 +2,14 @@
 
 set -ev
 
-PKG_DIR="${RECIPE_DIR}/.."
+PACKAGE_ROOT_DIR="${RECIPE_DIR}/.."
 
 python -m pytest \
-    -c "${PKG_DIR}/setup.cfg" \
-    --cov="${SP_DIR}/${PKG_NAME}" \
+    -c setup.cfg \
+    --cov="${SP_DIR}/${PKG_NAME}/${SUBPKG_NAME}" \
     --benchmark-disable \
-    "${PKG_DIR}"
+    --color=yes \
+    "tests/${SUBPKG_NAME}"
+
+sed -i "s|${SP_DIR}||g" .coverage
+mv .coverage "${PACKAGE_ROOT_DIR}/.coverage"
