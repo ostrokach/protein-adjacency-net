b3bc6a8010e871ee7d30c7ae4ed5a6a5a78fdd47
ostrokach <ostrokach@gmail.com>
2018-05-07 12:55:00 -0400

    Update notebooks

--- notebooks/evaluate_tim_barrel/slurm.sh
+++ notebooks/evaluate_tim_barrel/slurm.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d8e5d9c03eb2d1e0cff6edad74fb65bd15d3f3906f37a12d263153a12f53bd0
-size 605
+oid sha256:ed825a6ce152d207f941ee2753ec2b6158d9f90f1e2fd160fe4f8caad95fbc18
+size 642
