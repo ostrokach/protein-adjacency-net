20e742b196566dd096a29d8933a0cb37dab01515
ostrokach <ostrokach@gmail.com>
2018-06-15 18:43:07 -0400

    Update notebooks

---
+++ scripts/test_jupyter_extension.py
@@ -0,0 +1,16 @@
+# %% Imports
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+# %% Basic plot
+fg, axs = plt.subplots(figsize=(8, 6))
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.tight_layout()
+
+# %%
+print("hello world")
+
+# %% Hello world
+print("Done")
