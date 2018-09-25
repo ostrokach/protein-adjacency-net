7f28f8f874d8c81df13301a6d7d7b02242aa8f66
Alexey Strokach <ostrokach@gmail.com>
2018-09-25 12:02:58 -0400

    Write statistics to a sqlite database

--- pagnn/utils/stats.py
+++ pagnn/utils/stats.py
@@ -1,10 +1,26 @@
-from typing import Any, Dict
+from typing import Any, Callable, Dict, Optional
+
+import pandas as pd
+import sqlalchemy as sa
 
 
 class StatsBase:
-    step: int
-    extended: bool = False
-    scores: Dict[str, Any]
+    # Class attributes
+    columns_to_write: Dict[str, Optional[Callable]] = {}
 
-    def __init__(self, step: int) -> None:
+    # Instance attributes
+    step: int
+    _engine: sa.engine.Engine
+    scores: Dict[str, Any]
+    extended: bool
+
+    def __init__(self, step: int, engine: sa.engine.Engine) -> None:
         self.step = step
+        self._engine = engine
+
+    def _write_row(self):
+        data = {
+            k: fn(getattr(self, k)) if fn is not None else getattr(self, k)
+            for (k, fn) in self.columns_to_write.items()
+        }
+        df.to_sql("stats", self._engine, if_exists="append", index=False)
