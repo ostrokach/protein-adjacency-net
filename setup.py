8b1849f136aebf769af5e3b73f9f827a0e01e9e8
Alexey Strokach <ostrokach@gmail.com>
2018-09-28 01:22:03 -0400

    Use find_packages instead of hard-coding pagnn

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read_md(file):
@@ -14,7 +14,7 @@ setup(
     author="Alexey Strokach",
     author_email="alex.strokach@utoronto.ca",
     url="https://gitlab.com/kimlab/pagnn",
-    packages=["pagnn"],
+    packages=find_packages(),
     package_data={},
     include_package_data=True,
     zip_safe=False,
