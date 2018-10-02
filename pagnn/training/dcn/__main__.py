00c90112a5351f10dd72f6494c5b3c32807ed1f7
Alexey Strokach <ostrokach@gmail.com>
2018-10-02 01:50:04 -0400

    Display profiling info

--- pagnn/training/dcn/__main__.py
+++ pagnn/training/dcn/__main__.py
@@ -1,17 +1,40 @@
-import sys
-
-from .main import main
-
 # === Basic ===
-sys.exit(main())
+# import sys
+
+# from .main import main
+
+# sys.exit(main())
 
 # === Profiled ===
+from line_profiler import LineProfiler
+
+from pagnn.datavargan import dataset_to_datavar, push_adjs, push_seqs
+from pagnn.io import _read_random_row_group, iter_datarows
+
+from .main import main, train
+from .utils import basic_permuted_sequence_adder, generate_batch
+
+lp = LineProfiler()
+lp.add_function(dataset_to_datavar)
+lp.add_function(push_seqs)
+lp.add_function(push_adjs)
+lp.add_function(iter_datarows)
+lp.add_function(train)
+lp.add_function(basic_permuted_sequence_adder)
+lp.add_function(generate_batch)
+lp.add_function(_read_random_row_group)
+
+# Profile the main function
+lp_wrapper = lp(main)
+lp_wrapper()
+# Print results
+lp.print_stats()
+
+# ---
+# from memory_profiler import profile
 # with torch.autograd.profiler.profile() as prof:
 #     main()
 # print(prof)
-# from memory_profiler import profile
-# from line_profiler import LineProfiler
-# lp = LineProfiler()
 # # Add additional functions to profile
 # lp.add_function(score_edit)
 # lp.add_function(score_blosum62)
@@ -19,7 +42,6 @@ sys.exit(main())
 # lp.add_function(calculate_statistics_extended)
 # lp.add_function(evaluate_validation_dataset)
 # lp.add_function(evaluate_mutation_dataset)
-# lp.add_function(train)
 # # Profile the main function
 # lp_wrapper = lp(main)
 # lp_wrapper()
