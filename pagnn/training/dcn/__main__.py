54f213325f4ea4a1827c6fdb04504bdedba72036
ostrokach <ostrokach@gmail.com>
2018-10-03 11:47:54 -0400

    Disable profiling

--- pagnn/training/dcn/__main__.py
+++ pagnn/training/dcn/__main__.py
@@ -1,35 +1,35 @@
 # === Basic ===
-# import sys
+import sys
 
-# from .main import main
+from .main import main
 
-# sys.exit(main())
+sys.exit(main())
 
 # === Profiled ===
-from line_profiler import LineProfiler
+# from line_profiler import LineProfiler
 
-from pagnn.datavargan import dataset_to_datavar, gen_adj_pool, push_adjs, push_seqs
-from pagnn.io import _read_random_row_group, iter_datarows
+# from pagnn.datavargan import dataset_to_datavar, gen_adj_pool, push_adjs, push_seqs
+# from pagnn.io import _read_random_row_group, iter_datarows
 
-from .main import main, train
-from .utils import basic_permuted_sequence_adder, generate_batch
+# from .main import main, train
+# from .utils import basic_permuted_sequence_adder, generate_batch
 
-lp = LineProfiler()
-lp.add_function(dataset_to_datavar)
-lp.add_function(gen_adj_pool)
-lp.add_function(push_seqs)
-lp.add_function(push_adjs)
-lp.add_function(iter_datarows)
-lp.add_function(train)
-lp.add_function(basic_permuted_sequence_adder)
-lp.add_function(generate_batch)
-lp.add_function(_read_random_row_group)
+# lp = LineProfiler()
+# lp.add_function(dataset_to_datavar)
+# lp.add_function(gen_adj_pool)
+# lp.add_function(push_seqs)
+# lp.add_function(push_adjs)
+# lp.add_function(iter_datarows)
+# lp.add_function(train)
+# lp.add_function(basic_permuted_sequence_adder)
+# lp.add_function(generate_batch)
+# lp.add_function(_read_random_row_group)
 
-# Profile the main function
-lp_wrapper = lp(main)
-lp_wrapper()
-# Print results
-lp.print_stats()
+# # Profile the main function
+# lp_wrapper = lp(main)
+# lp_wrapper()
+# # Print results
+# lp.print_stats()
 
 # ---
 # from memory_profiler import profile
