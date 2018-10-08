2ca6bf6162da3c220089dc2512c76ea64adb4ffd
Alexey Strokach <ostrokach@gmail.com>
2018-10-08 11:44:24 -0400

    Be more specific when catching exceptions

--- pagnn/datapipe/pipebuf.py
+++ pagnn/datapipe/pipebuf.py
@@ -19,21 +19,21 @@ def _configure_buffer_sizes():
     # Teach the 'fcntl' module about 'F_SETPIPE_SZ', which is a Linux-ism,
     # but a good one that can drastically reduce the number of syscalls
     # when dealing with high-throughput pipes.
-    if not hasattr(fcntl, 'F_SETPIPE_SZ'):
+    if not hasattr(fcntl, "F_SETPIPE_SZ"):
         import platform
 
-        if platform.system() == 'Linux':
+        if platform.system() == "Linux":
             fcntl.F_SETPIPE_SZ = 1031
 
     # If Linux procfs (or something that looks like it) exposes its
     # maximum F_SETPIPE_SZ, adjust the default buffer sizes.
     try:
-        with open('/proc/sys/fs/pipe-max-size', 'r') as f:
+        with open("/proc/sys/fs/pipe-max-size", "r") as f:
             # Figure out OS pipe size, but in case it is unusually large
             # or small restrain it to sensible values.
             OS_PIPE_SZ = min(int(f.read()), 50 * 1024 * 1024)
             PIPE_BUF_BYTES = max(OS_PIPE_SZ, PIPE_BUF_BYTES)
-    except Exception:
+    except FileNotFoundError:
         pass
 
 
@@ -42,5 +42,5 @@ _configure_buffer_sizes()
 
 def set_buf_size(fd):
     """Set up os pipe buffer size, if applicable"""
-    if OS_PIPE_SZ and hasattr(fcntl, 'F_SETPIPE_SZ'):
+    if OS_PIPE_SZ and hasattr(fcntl, "F_SETPIPE_SZ"):
         fcntl.fcntl(fd, fcntl.F_SETPIPE_SZ, OS_PIPE_SZ)
