9f96fd5f8487fa337194d72b55d7c594fac7b2bb
ostrokach <ostrokach@gmail.com>
2018-11-21 14:24:58 -0500

    OUTPUT_DIR should contain CI_COMMIT_SHA

--- scripts/env_for_local_training.sh
+++ scripts/env_for_local_training.sh
@@ -1,6 +1,6 @@
 export SLURM_ARRAY_TASK_ID="0"
 export SBATCH_TIMELIMIT="1:00:00"
 export CI_COMMIT_SHA=$(git rev-parse HEAD)
-export OUTPUT_DIR="/tmp/strokach/${NETWORK_NAME}"
+export OUTPUT_DIR="/tmp/strokach/${CI_COMMIT_SHA}"
 mkdir -p ${OUTPUT_DIR}
 
