00e6ed100792b53632b4508028260a9dc9ed78c9
ostrokach <ostrokach@gmail.com>
2018-12-13 15:58:54 -0500

    Add get_adjacency_with_distances function

--- src/helper/__init__.py
+++ src/helper/__init__.py
@@ -1,5 +1,6 @@
 from .modeller_score import *
 from .common import *
+from .adjacency_matrix import *
 from .rosetta_score import *
 from .adjacency_matrix import *
 from .network_tools import *
--- src/helper/adjacency_matrix.py
+++ src/helper/adjacency_matrix.py
@@ -1,6 +1,7 @@
-from kmtools import structure_tools
+import pandas as pd
 
-from MDAnalysis.lib.distances import capped_distance
+from kmbio import PDB
+from kmtools import sequence_tools, structure_tools
 
 
 def get_interaction_dataset(structure, bioassembly_id=False, r_cutoff=5):
@@ -16,3 +17,64 @@ def get_interaction_dataset(structure, bioassembly_id=False, r_cutoff=5):
     #     interactions_core, interactions_core_aggbychain
     # )
     return interactions_core, interactions_core_aggbychain
+
+
+def get_adjacency_with_distances(
+    row, max_cutoff=12, min_cutoff=None, structure_url_prefix="rcsb://"
+):
+    # Load structure
+    url = f"{structure_url_prefix}{row.structure_id.lower()}.cif.gz"
+    structure = PDB.load(url)
+    # Template sequence
+    chain_sequence = structure_tools.get_chain_sequence(structure[row.model_id][row.chain_id])
+    template_sequence = chain_sequence[int(row.s_start - 1) : int(row.s_end)]
+    assert len(template_sequence) == len(row.a2b)
+    # Target sequence
+    target_sequence = row.sequence[int(row.q_start - 1) : int(row.q_end)]
+    assert len(target_sequence) == len(row.b2a)
+    # Extract domain
+    dd = structure_tools.DomainDef(row.model_id, row.chain_id, int(row.s_start), int(row.s_end))
+    domain = structure_tools.extract_domain(structure, [dd])
+    assert template_sequence == structure_tools.get_chain_sequence(domain)
+    assert template_sequence == row.sseq.replace('-', '')
+    # Get interactions
+    distances_core = structure_tools.get_distances(
+        domain, max_cutoff, min_cutoff, groupby="residue"
+    )
+    assert (distances_core["residue_idx_1"] <= distances_core["residue_idx_2"]).all()
+    # Map interactions to target
+    for i in [1, 2]:
+        distances_core[f"residue_idx_{i}_corrected"] = distances_core[f"residue_idx_{i}"].apply(
+            lambda idx: sequence_tools.convert_residue_index_a2b(idx, row.b2a)
+        )
+    # Remove missing values
+    distances_core = distances_core[
+        distances_core["residue_idx_1_corrected"].notnull()
+        & distances_core["residue_idx_2_corrected"].notnull()
+    ]
+    # Convert to integers
+    distances_core[["residue_idx_1_corrected", "residue_idx_2_corrected"]] = distances_core[
+        ["residue_idx_1_corrected", "residue_idx_2_corrected"]
+    ].astype(int)
+    # Sanity check
+    assert (
+        distances_core["residue_idx_1_corrected"] < distances_core["residue_idx_2_corrected"]
+    ).all()
+    # Get the set of interactions
+    interactions_1 = set(
+        distances_core[(distances_core["distance"] <= 5)][
+            ["residue_idx_1_corrected", "residue_idx_2_corrected"]
+        ].apply(tuple, axis=1)
+    )
+    # Get the reference set of interactions
+    interactions_2 = {
+        (int(r1), int(r2)) if r1 <= r2 else (int(r2), int(r1))
+        for r1, r2 in zip(row.residue_idx_1_corrected, row.residue_idx_2_corrected)
+        if pd.notnull(r1) and pd.notnull(r2)
+    }
+    assert not interactions_1 ^ interactions_2
+    return (
+        distances_core["residue_idx_1_corrected"].values,
+        distances_core["residue_idx_2_corrected"].values,
+        distances_core["distance"].values,
+    )
