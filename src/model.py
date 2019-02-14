052c8544d285bdc00dc3c8cc69bfcfc01d3ca4f5
Alexey Strokach <ostrokach@gmail.com>
2019-02-14 00:41:26 -0500

    max_distance=8

--- src/model.py
+++ src/model.py
@@ -437,7 +437,7 @@ class Custom(nn.Module):
                 bias=False,
                 wself=True,
                 barcode_method="combined.pretrained",
-                max_distance=10,
+                max_distance=8,
             ),
             nn.ReLU(),
         )
