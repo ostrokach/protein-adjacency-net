852af36eda8ec26b743640306fb28990641d46e3
Alexey Strokach <alex.strokach@utoronto.ca>
2021-03-23 18:03:22 +0000

    Update evaluate_tim_barrel files

--- notebooks/evaluate_tim_barrel/slurm.sh
+++ notebooks/evaluate_tim_barrel/slurm.sh
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ed825a6ce152d207f941ee2753ec2b6158d9f90f1e2fd160fe4f8caad95fbc18
-size 642
+#!/bin/bash
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --mem=0
+#SBATCH --account=rrg-pmkim
+#SBATCH --job-name=pagnn-validation
+#SBATCH --export=ALL
+
+set -ev
+
+unset XDG_RUNTIME_DIR
+
+if [[ -z ${MY_JOB_IDX} ]] ; then
+    echo "MY_JOB_IDX environment variable has to be set!"
+    exit 1
+fi
+
+python ~/working/pagnn/pagnn/prediction/gan/validation.py \
+    --input-file ./1vkf_adjacencies.parquet \
+    --output-file ./validation${MY_JOB_IDX}.parquet \
+    --work-path ~/datapkg/adjacency-net/notebooks/train_neural_network/gan-permute-seq-0-test_x12-0.1.9.dev-4a07eef/ \
+    --step 5900 \
+    --with-ss \
+    --nseqs 1000
