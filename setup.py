89f0de8f03e1b5b6f3c999344bc3a8e0b7280ed4
Alexey Strokach <alex.strokach@utoronto.ca>
2021-03-23 22:32:09 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-def read_md(file):
+def read_file(file):
     with open(file) as fin:
         return fin.read()
 
@@ -10,20 +10,22 @@ setup(
     name="pagnn",
     version="0.1.14",
     description="Protein Adjacency Graph Neural Network.",
-    long_description=read_md("README.md"),
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
     author="Alexey Strokach",
     author_email="alex.strokach@utoronto.ca",
-    url="https://gitlab.com/kimlab/pagnn",
+    url="https://gitlab.com/ostrokach/protein-adjacency-net",
     packages=find_packages("src"),
-    package_data={"pagnn": "model_data/*.state", "pagnn.training": "data/*"},
+    package_dir={"": "src"},
+    package_data={"pagnn": ["model_data/*.state", "training/data/*"]},
     include_package_data=True,
     zip_safe=False,
     keywords="pagnn",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite="tests",
 )
